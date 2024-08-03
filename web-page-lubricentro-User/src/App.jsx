ee69afe6ac7d920575786b4ebdf1f47451ecd057
Brandon2395 <acostabrandon2395@gmail.com>
2024-08-03 13:06:27 -0600

    Update Code
    Código Usuario

---
+++ web-page-lubricentro-User/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import AppHeader from './components/Login/header';
+
+
+function App() {
+    return (
+        <div className="App">
+            <header id="header">
+                <AppHeader />
+            </header>
+        </div>
+    );
+}
+
+export default App;
