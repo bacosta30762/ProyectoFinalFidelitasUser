76106f40f8e94d06fefff8363f198642f70793f5
Brandon2395 <acostabrandon2395@gmail.com>
2024-08-11 16:03:13 -0600

    update código

--- web-page-lubricentro-User/src/App.jsx
+++ web-page-lubricentro-User/src/App.jsx
@@ -1,9 +1,7 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import AppHeader from './components/Login/header'
-import IngresosPage from './components/Contability/Ingresos'
-//import EgresosPage from './components/Contability/Egresos'
-//import ReportesPage from './components/Contability/Reportes'
+
 
 function App() {
     return (
@@ -11,7 +9,6 @@ function App() {
             <div className="App">
                 <AppHeader />
                 <Routes>
-                    <Route path="/ingresos" element={<IngresosPage />} />
                     <Route path="/" element={<h1>Home</h1>} />
                 </Routes>
             </div>
--- web-page-lubricentro-User/src/components/Login/header.js
+++ web-page-lubricentro-User/src/components/Login/header.js
@@ -1,7 +1,6 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 import { Link } from 'react-router-dom';
 
 export default function AppHeader() {
@@ -14,11 +13,6 @@ export default function AppHeader() {
                     <Nav className="ms-auto">
                         <Nav.Link as={Link} to="/">Home</Nav.Link>
                         <Nav.Link as={Link} to="/link">Link</Nav.Link>
-                        <NavDropdown title="Contabilidad" id="basic-nav-dropdown">
-                            <NavDropdown.Item as={Link} to="/ingresos">Ingresos</NavDropdown.Item>
-                            <NavDropdown.Item as={Link} to="/egresos">Egresos</NavDropdown.Item>
-                            <NavDropdown.Item as={Link} to="/reportes">Reportes Financieros</NavDropdown.Item>
-                        </NavDropdown>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
