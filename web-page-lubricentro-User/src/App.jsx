4e5c83da2b7ecaca5021a0366dbc9f51bd539947
Brandon2395 <acostabrandon2395@gmail.com>
2024-08-12 19:21:24 -0600

    Home

--- web-page-lubricentro-User/src/App.jsx
+++ web-page-lubricentro-User/src/App.jsx
@@ -4,16 +4,11 @@ import AppHeader from "./components/HeaderFooter/header";
 import OrdenPage from "./pages/OrdenPage";
 import UserNotificationsPage from "./pages/UserNotificationsPage";
 import ComentariosValoraciones from "./components/Comentarios/ComentariosValoraciones";
-import Footer from "./components/HeaderFooter/Footer"; 
+import Footer from "./components/HeaderFooter/Footer";
 import Login from "./components/Login/login.jsx";
-import Registro from './components/Login/registro.jsx';
-import RecuperarContra from './components/Login/recuperarcontra.jsx';
-
-
-   
-  
-
-
+import Registro from "./components/Login/registro.jsx";
+import RecuperarContra from "./components/Login/recuperarcontra.jsx";
+import Home from "./components/Home/Home";
 
 function App() {
   return (
@@ -23,9 +18,10 @@ function App() {
           <AppHeader />
         </header>
         <Routes>
-        <Route path="/Login/" element={<Login/>} />
-        <Route path="/RecuperarContra/" element={<RecuperarContra />} />
-        <Route path="/Registro/" element={<Registro />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/Login/" element={<Login />} />
+          <Route path="/RecuperarContra/" element={<RecuperarContra />} />
+          <Route path="/Registro/" element={<Registro />} />
           <Route path="/orden/*" element={<OrdenPage />} />
           <Route path="notifications" element={<UserNotificationsPage />} />
           <Route
@@ -35,8 +31,8 @@ function App() {
         </Routes>
       </Router>
       <footer id="footer">
-          <Footer />
-        </footer>
+        <Footer />
+      </footer>
     </div>
   );
 }
