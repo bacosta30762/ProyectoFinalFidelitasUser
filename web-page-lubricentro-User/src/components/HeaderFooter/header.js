41b3280edb4ed9112df3813751dd0bb591e97b9d
SaulRet <sau1585@hotmail.com>
2024-08-13 16:27:43 -0600

    notificaciones
    implementacion de notificaciones al usuario

--- web-page-lubricentro-User/src/components/HeaderFooter/header.js
+++ web-page-lubricentro-User/src/components/HeaderFooter/header.js
@@ -1,12 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import { NavDropdown, Dropdown, Badge } from "react-bootstrap";
+import { Bell } from "react-bootstrap-icons";
 import "./AppHeader.css"; // Importa el archivo CSS personalizado
-import { NavDropdown } from "react-bootstrap";
 
 export default function AppHeader() {
+  const [showNotifications, setShowNotifications] = useState(false);
+
+  const handleToggleNotifications = () => {
+    setShowNotifications(!showNotifications);
+  };
+
   return (
     <Navbar expand="lg" className="bg-rojizo">
       <Container>
@@ -27,15 +34,44 @@ export default function AppHeader() {
             <Nav.Link as={NavLink} to="/calendario">
               Disponibilidad Citas
             </Nav.Link>
-            <Nav.Link as={NavLink} to="/notifications">
-              Notificaciones
-            </Nav.Link>
             <Nav.Link as={NavLink} to="/comentarios-valoraciones">
               Opiniones
             </Nav.Link>
             <Nav.Link as={NavLink} to="/Login">
               Iniciar Sesión
             </Nav.Link>
+            <Dropdown
+              align="end"
+              show={showNotifications}
+              onToggle={handleToggleNotifications}
+            >
+              <Dropdown.Toggle
+                as="div"
+                id="notification-bell"
+                onClick={handleToggleNotifications}
+                style={{
+                  cursor: "pointer",
+                  display: "flex",
+                  alignItems: "center",
+                }}
+              >
+                <Bell size={20} />
+                <Badge pill bg="danger" style={{ marginLeft: "5px" }}>
+                  3
+                </Badge>
+              </Dropdown.Toggle>
+              <Dropdown.Menu>
+                <Dropdown.Item>
+                  🎉 Promoción: 20% de descuento en servicios de aceite
+                </Dropdown.Item>
+                <Dropdown.Item>
+                  🕒 Horarios: Lunes a Viernes de 8 AM a 6 PM
+                </Dropdown.Item>
+                <Dropdown.Item>
+                  🔔 Recordatorio: Cita programada para el 20 de agosto
+                </Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
           </Nav>
         </Navbar.Collapse>
       </Container>
